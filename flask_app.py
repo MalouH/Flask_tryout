25cd04614ec9326606f1c40fb31076083a13f510
Malou <malouhessel@hotmail.com>
2023-10-20 15:51:50 +0200

    tekst naar keuze toegevoegd

---
+++ flask_app.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app=Flask (__name__)
+@app.route('/')
+def home():
+    return "<p>Hallo dit is mijn tekst naar keuze</p>"
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
